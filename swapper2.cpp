926c5a6160d84960cb55f882d3b74bca3b3361a4
Davorino7 <269927664+Davorino7@users.noreply.github.com>
2026-06-11 11:22:55 +0800

    Add files via upload

---
+++ swapper2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void swapPointers (int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+
+}
+int main () {
+    
+    int x, y;
+
+    cout << "Enter value for x: ";
+    cin >> x;
+    cout << "Enter value for y: ";
+    cin >> y;
+
+    cout << "You entered: x = " << x << ", y =  " << y;
+
+    swapPointers (x, y);
+        cout << "\nResult: x = " << x << ", y = " << y;
+
+
+    return 0;
+}
